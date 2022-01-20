2898629ff160f42faf54dbb38ccebd966a370a0c
ryan <ryanmrichard1@gmail.com>
2022-01-20 13:30:51 -0600

    fixes bug with ordinal index

--- tests/tensor/conversions.cpp
+++ tests/tensor/conversions.cpp
@@ -24,4 +24,16 @@ TEST_CASE("to_vector") {
         std::vector<double> corr{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0};
         REQUIRE(to_vector(t) == corr);
     }
+
+    // The conversion previously had a bug that incorrectly computed the
+    // flattened offset when the tensor had more than one tile. This test
+    // ensures that bug doesn't come back
+    SECTION("More than one tile") {
+        using field_t          = field::Scalar;
+        using single_element_t = SingleElementTiles<field_t>;
+        auto palloc            = std::make_unique<single_element_t>();
+        tensor_type t(tensors.at("matrix"), std::move(palloc));
+        std::vector<double> corr{1.0, 2.0, 3.0, 4.0};
+        REQUIRE(to_vector(t) == corr);
+    }
 }
