829941739835ca4ae81fcc8187906d22245ec2cc
Jonathan M. Waldrop <relik107@gmail.com>
2022-02-07 13:07:45 -0600

    keep tile logic

--- src/tensorwrapper/sparse_map/sparse_map/detail_/tiling_map_index.hpp
+++ src/tensorwrapper/sparse_map/sparse_map/detail_/tiling_map_index.hpp
@@ -1,112 +1,169 @@
 #include "tensorwrapper/sparse_map/index.hpp"
 #include "tensorwrapper/sparse_map/sparse_map/sparse_map.hpp"
+#include <TiledArray/tiled_range.h>
 
 namespace tensorwrapper::sparse_map::detail_ {
 
-sparse_map::Index elems_to_tile();
+/** @brief Returns a sparse map with independent elements based on tiles.
+ *
+ *  Produce a new sparse map whose independent elements are the independent
+ *  elements of another map converted to tile indices based on the provided
+ *  tiled range.
+ *
+ *  @param[in] sm The original sparse map
+ *  @param[in] tr The tiled range used to convert the elements
+ *
+ *  @return The new sparse map with tile based independent indices.
+ *
+ *  @throw std::runtime_error if the rank of the tiled range does not match the
+ *                            rank of the independent indices.
+ */
+inline sparse_map::SparseMap tile_independent_indices(
+  const sparse_map::SparseMap& sm, const TA::TiledRange& tr) {
+    if(tr.rank() != sm.ind_rank())
+        throw std::runtime_error("Rank of TiledRange does not equal"
+                                 "independent index rank");
+    sparse_map::SparseMap new_sm;
+    for(const auto& [ind_idx, d] : sm) {
+        auto temp = tr.tiles_range().idx(tr.element_to_tile(ind_idx));
+        sparse_map::Index new_idx(temp.begin(), temp.end());
+        for(const auto& dep_idx : d) { new_sm.add_to_domain(new_idx, dep_idx); }
+    }
+    return new_sm;
+}
 
-/// TODO: Move logic below into free functions
+/** @brief Returns a sparse map with dependent elements based on tiles.
+ *
+ *  Produce a new sparse map whose dependent elements are the dependent
+ *  elements of another map converted to tile indices based on the provided
+ *  tiled range.
+ *
+ *  @param[in] sm The original sparse map
+ *  @param[in] tr The tiled range used to convert the elements
+ *
+ *  @return The new sparse map with tile based dependent indices.
+ *
+ *  @throw std::runtime_error if the rank of the tiled range does not match the
+ *                            rank of the dependent indices.
+ */
+sparse_map::SparseMap tile_dependent_indices(const sparse_map::SparseMap& sm,
+                                             const TA::TiledRange& tr) {
+    if(tr.rank() != sm.dep_rank())
+        throw std::runtime_error("Rank of TiledRange does not equal"
+                                 "dependent index rank");
+    sparse_map::SparseMap new_sm;
+    for(const auto& [ind_idx, d] : sm) {
+        for(const auto& dep_idx : d) {
+            auto temp = tr.tiles_range().idx(tr.element_to_tile(dep_idx));
+            sparse_map::Index new_idx(temp.begin(), temp.end());
+            new_sm.add_to_domain(ind_idx, new_idx);
+        }
+    }
+    return new_sm;
+}
 
-// inline void check_tr(std::size_t rank, const TA::TiledRange& tr) {
-//     const bool is_not_set = tr == TA::TiledRange{};
-//     if(rank > 0 && is_not_set)
-//         throw std::runtime_error("Input SparseMap's TiledRange is not set");
-// }
+/** @brief Returns a sparse map with elements based on tiles.
+ *
+ *  Produce a new sparse map whose elements are the elements of another map
+ *  converted to tile indices based on the provided tiled range.
+ *
+ *  @param[in] sm The original sparse map
+ *  @param[in] ind_tr The tiled range used to convert the independent elements
+ *  @param[in] dep_tr The tiled range used to convert the dependent elements
+ *
+ *  @return The new sparse map with tile based indices.
+ *
+ *  @throw std::runtime_error if the ranks of the tiled range does not match the
+ *                            ranks of the indices.
+ */
+sparse_map::SparseMap tile_indices(const sparse_map::SparseMap& sm,
+                                   const TA::TiledRange& ind_tr,
+                                   const TA::TiledRange& dep_tr, ) {
+    auto intermediate_sm = tile_independent_indices(sm, ind_tr);
+    return tile_dependent_indices(intermediate_sm, dep_tr);
+}
 
-// SPARSEMAPEE::SparseMap(const SPARSEMAPET& other) {
-//     for(const auto& [oeidx, d] : other) {
-//         for(const auto& itidx : d) {
-//             const auto tr = d.trange();
-//             check_tr(other.dep_rank(), tr);
-//             for(const auto& ieidx : tr.make_tile_range(itidx)) {
-//                 const ElementIndex temp(ieidx.begin(), ieidx.end());
-//                 add_to_domain(oeidx, temp);
-//             }
-//         }
-//     }
-// }
+/** @brief Returns a sparse map with independent elements converted from tile to
+ *         element indices
+ *
+ *  Produce a new sparse map whose independent indices are the independent
+ *  indices of another map converted to elements based on the provided
+ *  tiled range.
+ *
+ *  @param[in] sm The original sparse map
+ *  @param[in] tr The tiled range used to convert the elements
+ *
+ *  @return The new sparse map with element based independent indices.
+ *
+ *  @throw std::runtime_error if the rank of the tiled range does not match the
+ *                            rank of the independent indices.
+ */
+inline sparse_map::SparseMap untile_independent_indices(
+  const sparse_map::SparseMap& sm, const TA::TiledRange& tr) {
+    if(tr.rank() != sm.ind_rank())
+        throw std::runtime_error("Rank of TiledRange does not equal"
+                                 "independent index rank");
+    sparse_map::SparseMap new_sm;
+    for(const auto& [ind_idx, d] : sm) {
+        for(const auto& new_idx : tr.make_tile_range(ind_idx)) {
+            for(const auto& dep_idx : d) {
+                new_sm.add_to_domain(new_idx, dep_idx);
+            }
+        }
+    }
+    return new_sm;
+}
 
-// SPARSEMAPEE::SparseMap(const SPARSEMAPTE& other) {
-//     const auto& trange = other.trange();
-//     check_tr(other.ind_rank(), trange);
-//     for(const auto& [otidx, d] : other) {
-//         for(const auto& oeidx : trange.make_tile_range(otidx)) {
-//             for(const auto& ieidx : d) {
-//                 const ElementIndex temp(oeidx.begin(), oeidx.end());
-//                 add_to_domain(temp, ieidx);
-//             }
-//         }
-//     }
-// }
+/** @brief Returns a sparse map with dependent elements converted from tile to
+ *         element indices
+ *
+ *  Produce a new sparse map whose dependent indices are the dependent
+ *  indices of another map converted to elements based on the provided
+ *  tiled range.
+ *
+ *  @param[in] sm The original sparse map
+ *  @param[in] tr The tiled range used to convert the elements
+ *
+ *  @return The new sparse map with element based dependent indices.
+ *
+ *  @throw std::runtime_error if the rank of the tiled range does not match the
+ *                            rank of the dependent indices.
+ */
+inline sparse_map::SparseMap untile_dependent_indices(
+  const sparse_map::SparseMap& sm, const TA::TiledRange& tr) {
+    if(tr.rank() != sm.dep_rank())
+        throw std::runtime_error("Rank of TiledRange does not equal"
+                                 "dependent index rank");
+    sparse_map::SparseMap new_sm;
+    for(const auto& [ind_idx, d] : sm) {
+        for(const auto& dep_idx : d) {
+            for(const auto& new_idx : tr.make_tile_range(dep_idx)) {
+                new_sm.add_to_domain(ind_idx, new_idx);
+            }
+        }
+    }
+    return new_sm;
+}
 
-// SPARSEMAPEE::SparseMap(const SPARSEMAPTT& other) {
-//     const auto& trange = other.trange();
-//     check_tr(other.ind_rank(), trange);
-
-//     for(const auto& [otidx, d] : other) {
-//         for(const auto& oeidx : trange.make_tile_range(otidx)) {
-//             const ElementIndex otemp(oeidx.begin(), oeidx.end());
-//             for(const auto& itidx : d) {
-//                 check_tr(other.dep_rank(), d.trange());
-//                 for(const auto& ieidx : d.trange().make_tile_range(itidx)) {
-//                     const ElementIndex itemp(ieidx.begin(), ieidx.end());
-//                     add_to_domain(otemp, itemp);
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// SPARSEMAPET::SparseMap(const TA::TiledRange& tr, const SPARSEMAPEE& other) {
-//     for(const auto& [ind, dep] : other) {
-//         for(auto tidx : TileDomain(tr, dep)) add_to_domain(ind, tidx);
-//     }
-//     set_domain_trange(tr);
-// }
-
-// SPARSEMAPTE::SparseMap(const SPARSEMAPTT& other) {
-//     set_trange(other.trange());
-//     for(const auto& [otidx, d] : other) {
-//         const auto& itrange = d.trange();
-//         check_tr(other.dep_rank(), itrange);
-//         for(const auto& itidx : d) {
-//             for(const auto& ieidx : itrange.make_tile_range(itidx)) {
-//                 ElementIndex new_idx(ieidx.begin(), ieidx.end());
-//                 add_to_domain(otidx, std::move(new_idx));
-//             }
-//         }
-//     }
-// }
-
-// SPARSEMAPTE::SparseMap(const TA::TiledRange& trange, const SPARSEMAPEE&
-// other) {
-//     if(trange.rank() != other.ind_rank())
-//         throw std::runtime_error("Rank of TiledRange does not equal
-//         independent"
-//                                  " index rank");
-//     set_trange(trange);
-//     for(const auto& [oeidx, d] : other) {
-//         auto otemp = trange.tiles_range().idx(trange.element_to_tile(oeidx));
-//         TileIndex otidx(otemp.begin(), otemp.end());
-//         for(const auto& ieidx : d) { add_to_domain(otidx, ieidx); }
-//     }
-// }
-
-// SPARSEMAPTT::SparseMap(const TA::TiledRange& trange, const SPARSEMAPET&
-// other) {
-//     if(trange.rank() != other.ind_rank())
-//         throw std::runtime_error("Rank of TiledRange does not equal
-//         independent"
-//                                  " index rank");
-//     set_trange(trange);
-//     std::optional<TA::TiledRange> dom_tr;
-//     for(const auto& [oeidx, d] : other) {
-//         if(!dom_tr.has_value() && d.trange().rank()) dom_tr = d.trange();
-//         auto otemp = trange.tiles_range().idx(trange.element_to_tile(oeidx));
-//         TileIndex otidx(otemp.begin(), otemp.end());
-//         for(const auto& ieidx : d) { add_to_domain(otidx, ieidx); }
-//     }
-//     if(dom_tr.has_value()) set_domain_trange(dom_tr.value());
-// }
+/** @brief Returns a sparse map with indices based on tensor elements.
+ *
+ *  Produce a new sparse map whose indices are the indices of another map
+ *  converted to elements based on the provided tiled range.
+ *
+ *  @param[in] sm The original sparse map
+ *  @param[in] ind_tr The tiled range used to convert the independent elements
+ *  @param[in] dep_tr The tiled range used to convert the dependent elements
+ *
+ *  @return The new sparse map with element based indices.
+ *
+ *  @throw std::runtime_error if the ranks of the tiled range does not match the
+ *                            ranks of the indices.
+ */
+sparse_map::SparseMap untile_indices(const sparse_map::SparseMap& sm,
+                                     const TA::TiledRange& ind_tr,
+                                     const TA::TiledRange& dep_tr, ) {
+    auto intermediate_sm = untile_independent_indices(sm, ind_tr);
+    return untile_dependent_indices(intermediate_sm, dep_tr);
+}
 
 } // namespace tensorwrapper::sparse_map::detail_
