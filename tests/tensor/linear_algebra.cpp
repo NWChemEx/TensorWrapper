c964a7b3b8c13cb58a129d49efa3d070f963abd8
Jonathan M. Waldrop <relik107@gmail.com>
2022-04-19 11:32:08 -0500

    svd tests

--- tests/tensor/linear_algebra.cpp
+++ tests/tensor/linear_algebra.cpp
@@ -12,6 +12,19 @@ TA::detail::matrix_il<double> evec_data{
   {0.3279852776056812, -0.7369762290995785, 0.5910090485061033},
   {-0.5910090485061031, 0.3279852776056821, 0.7369762290995784}};
 
+TA::detail::vector_il<double> svd_values{14.2274074126337418,
+                                         1.2573298353791105};
+
+TA::detail::matrix_il<double> svd_left{
+  {-0.3761682344281408, -0.9265513797988839},
+  {-0.9265513797988839, 0.3761682344281408}};
+
+TA::detail::matrix_il<double> svd_right{
+  {-0.3520616924890126, -0.4436257825895202, -0.5351898726900277,
+   -0.6267539627905352},
+  {0.7589812676751461, 0.3212415991459322, -0.1164980693832819,
+   -0.5542377379124960}};
+
 } // namespace
 
 TEST_CASE("eigen_solve") {
@@ -48,24 +61,51 @@ TEST_CASE("eigen_solve") {
 TEST_CASE("SVD") {
     using ivector_il = TA::detail::vector_il<int>;
     using imatrix_il = TA::detail::matrix_il<int>;
-    using dvector_il = TA::detail::vector_il<double>;
-    using dmatrix_il = TA::detail::matrix_il<double>;
     using TWrapper   = ScalarTensorWrapper;
     auto& world      = TA::get_default_world();
-    TA::TSpArrayD data1(world, imatrix_il{ivector_il{1, 2, 3, 8},
-                                          ivector_il{2, 4, 5, 9},
-                                          ivector_il{3, 5, 6, 0}});
-    TWrapper X, Y(data1);
-    X("x,y") = Y("y,x");
 
-    const auto& S0            = SVDValues(X);
-    const auto& [S1, U1]      = SVDLeft(X);
-    const auto& [S2, VT2]     = SVDRight(X);
-    const auto& [S3, U3, VT3] = SVD(X);
+    TWrapper values_corr(TA::TSpArrayD(world, svd_values));
+    TWrapper left_corr(TA::TSpArrayD(world, svd_left));
+    TWrapper right_corr(TA::TSpArrayD(world, svd_right));
+    TWrapper X(TA::TSpArrayD(
+      world, imatrix_il{ivector_il{1, 2, 3, 4}, ivector_il{5, 6, 7, 8}}));
 
-    REQUIRE(allclose(S0, S1));
-    REQUIRE(allclose(S0, S2));
-    REQUIRE(allclose(S0, S3));
-    REQUIRE(allclose(U1, U3));
-    REQUIRE(allclose(VT2, VT3));
+    SECTION("Values") {
+        const auto& S = SVDValues(X);
+        REQUIRE(allclose(S, values_corr));
+    }
+
+    SECTION("Left") {
+        const auto& [S, U] = SVDLeft(X);
+        REQUIRE(allclose(S, values_corr));
+        REQUIRE(abs_allclose(U, left_corr));
+    }
+
+    SECTION("Right") {
+        const auto& [S, VT] = SVDRight(X);
+        REQUIRE(allclose(S, values_corr));
+        REQUIRE(abs_allclose(VT, right_corr));
+    }
+
+    SECTION("All") {
+        const auto& [S, U, VT] = SVD(X);
+        REQUIRE(allclose(S, values_corr));
+        REQUIRE(abs_allclose(U, left_corr));
+        REQUIRE(abs_allclose(VT, right_corr));
+    }
+
+    SECTION("Transpose Matrix") {
+        // Make input tall instead of wide
+        X("y,x") = X("x,y");
+
+        // Make transposed vectors.
+        TWrapper alt_left_corr, alt_right_corr;
+        alt_right_corr("y,x")  = left_corr("x,y");
+        alt_left_corr("y,x") = right_corr("x,y");
+
+        const auto& [S, U, VT] = SVD(X);
+        REQUIRE(allclose(S, values_corr));
+        REQUIRE(abs_allclose(U, alt_left_corr));
+        REQUIRE(abs_allclose(VT, alt_right_corr));
+    }
 }
