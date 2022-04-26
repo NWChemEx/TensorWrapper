ccdb0246cf277af04e9dba91cb0fdb68705b8579
Clang Robot <robot@example.com>
2022-04-26 17:04:33 +0000

    Committing clang-format changes

--- tests/tensor/novel/shapes/shape.cpp
+++ tests/tensor/novel/shapes/shape.cpp
@@ -54,33 +54,33 @@ TEST_CASE("Shape<Scalar>") {
             REQUIRE(*pv == vector);
         }
 
-	SECTION("Copy") {
+        SECTION("Copy") {
             shape_type cpy(vector);
-	    REQUIRE(cpy == vector);
-	}
+            REQUIRE(cpy == vector);
+        }
 
-	SECTION("Move") {
+        SECTION("Move") {
             shape_type cpy(vector);
-	    shape_type mv(std::move(cpy));
-	    REQUIRE(mv == vector);
-	    REQUIRE(cpy == defaulted);
-	}
+            shape_type mv(std::move(cpy));
+            REQUIRE(mv == vector);
+            REQUIRE(cpy == defaulted);
+        }
     }
 
     SECTION("Assignment") {
         shape_type cpy(matrix);
-	REQUIRE(cpy != vector);
+        REQUIRE(cpy != vector);
         SECTION("Copy") {
-	    cpy = vector;
-	    REQUIRE(cpy == vector);
-	}
+            cpy = vector;
+            REQUIRE(cpy == vector);
+        }
 
-	SECTION("Move") {
-	    shape_type mv(matrix);
-	    mv = std::move(cpy);
-	    REQUIRE(mv == matrix);
-	    REQUIRE(cpy == defaulted);
-	}
+        SECTION("Move") {
+            shape_type mv(matrix);
+            mv = std::move(cpy);
+            REQUIRE(mv == matrix);
+            REQUIRE(cpy == defaulted);
+        }
     }
 
     SECTION("extents") {
