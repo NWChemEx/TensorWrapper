b86f9f860bd24f3a3059f670dcec13bfed1205ab
LChenGit <lechen@iastate.edu>
2022-05-16 10:42:00 -0500

    fix compile; add fence; add mult section

--- tests/performance_tests/tw_vs_ta.cpp
+++ tests/performance_tests/tw_vs_ta.cpp
@@ -1,78 +1,47 @@
-#include "performance_tests.hpp"
+// #include "tensorwrapper/tensor/tensor.hpp"
+#include <catch2/catch.hpp>
+#include <chrono>
+#include <iostream>
+#include <tiledarray.h>
+using namespace std::chrono;
+#include "../tensor/test_tensor.hpp"
 
-using namespace performance_tests;
-
-// generate two random dummy tensors for test
-namespace {
-std::initializer_list<double> l1{rand_d, rand_d, rand_d, rand_d};
-std::initializer_list<double> l2{rand_d, rand_d, rand_d, rand_d};
-} // namespace
+using namespace tensorwrapper;
+using namespace tensorwrapper::tensor;
+using scalar_traits  = backends::TiledArrayTraits<field::Scalar>;
+using scalar_variant = typename scalar_traits::variant_type;
+using scalar_tensor  = typename scalar_traits::tensor_type<double>;
 
 TEST_CASE("Performance test: TensorWrapper v.s. TiledArray") {
-    // using tensor_t = tensorwrapper::tensor::ScalarTensorWrapper;
-    // auto tensor = performance_tests::gen_tensors<TA::TSpArrayD>();
+    SECTION("mult") {
+        auto lhs = testing::get_tensors<scalar_tensor>().at("vector");
+        auto rhs = testing::get_tensors<scalar_tensor>().at("vector");
+        // madness::initialized();
+        auto& world = TA::get_default_world();
+        scalar_tensor res_ta;
+        world.gop.fence();
+        auto start_ta = high_resolution_clock::now();
+        res_ta("i,j") = lhs("i") * rhs("j");
+        world.gop.fence();
+        auto stop_ta     = high_resolution_clock::now();
+        auto duration_ta = duration_cast<microseconds>(stop_ta - start_ta);
 
-    SECTION("Multiplication") {
-        // TA session
-        TA::TArrayD lhs_tensor{world, l1};
-        TA::TArrayD rhs_tensor{world, l2};
-        // fill the argument arrays with data
-        // lhs_t.fill_local(rand_d);
-        // lhs_t.fill_local(rand_d);
-        TA::TArrayD res_ta;
-        // run multiplication
-        auto begin_ta = std::chrono::high_resolution_clock::now();
-        res_ta("i,j") = lhs_tensor("i,k") * rhs_tensor("k, j");
-        std::cout << 'TiledArray running time: ' << begin_ta.time_since_epoch()
-                  << std::endl;
+        std::cout << "performance test:" << std::endl;
+        std::cout << "Time taken by operation(TA): " << duration_ta.count()
+                  << " microseconds" << std::endl;
 
-        // TW session
-        tensorwrapper::tensor::detail_::MultOp<std::initializer_list<double>,
-                                               std::initializer_list<double>>
-          add_tw;
-        auto begin_tw = std::chrono::high_resolution_clock::now();
-        auto res_tw   = add_tw.MultOp(l1, l2);
-        std::cout << 'TensorWrapper running time: '
-                  << begin_tw.time_since_epoch() << std::endl;
-    }
+        ScalarTensorWrapper wrapped_lhs(lhs);
+        ScalarTensorWrapper wrapped_rhs(rhs);
+        ScalarTensorWrapper result(scalar_tensor{});
+        world.gop.fence();
+        auto start_tw  = high_resolution_clock::now();
+        result("i, j") = wrapped_lhs("i") * wrapped_rhs("j");
+        world.gop.fence();
+        auto stop_tw     = high_resolution_clock::now();
+        auto duration_tw = duration_cast<microseconds>(stop_tw - start_tw);
 
-    SECTION("Add") {
-        TA::TArrayD lhs_tensor{world, l1};
-        TA::TArrayD rhs_tensor{world, l2};
-        TA::TArrayD res_ta;
-        // TA session
-        auto begin_ta = std::chrono::high_resolution_clock::now();
-        res_ta("i,j") = lhs_tensor("i,j") + rhs_tensor("i, j");
-        std::cout << 'TiledArray running time: ' << begin_ta.time_since_epoch()
-                  << std::endl;
+        std::cout << "Time taken by operation(TW): " << duration_tw.count()
+                  << " microseconds" << std::endl;
 
-        // TW session
-        tensorwrapper::tensor::detail_::AddOp<std::initializer_list<double>,
-                                              std::initializer_list<double>>
-          add_tw;
-        auto begin_tw = std::chrono::high_resolution_clock::now();
-        auto res_tw   = add_tw.AddOp(l1, l2);
-        std::cout << 'TensorWrapper running time: '
-                  << begin_tw.time_since_epoch() << std::endl;
-    }
-
-    SECTION("Substract") {
-        TA::TArrayD lhs_tensor{world, l1};
-        TA::TArrayD rhs_tensor{world, l2};
-        TA::TArrayD res_ta;
-        // TA session
-        auto begin_ta = std::chrono::high_resolution_clock::now();
-        res_ta("i,j") = lhs_tensor("i,j") - rhs_tensor("i, j");
-        std::cout << 'TiledArray running time: ' << begin_ta.time_since_epoch()
-                  << std::endl;
-
-        // TW session
-        tensorwrapper::tensor::detail_::SubtOp<std::initializer_list<double>,
-                                               std::initializer_list<double>>
-          sub_tw;
-        auto begin_tw = std::chrono::high_resolution_clock::now();
-        auto res_tw   = sub_tw.SubtOp(l1, l2);
-        std::cout << 'TensorWrapper running time: '
-                  << begin_tw.time_since_epoch() << std::endl;
     }
 }
