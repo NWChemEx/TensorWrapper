d1ea9339791dc812b95d86fdfcc312528f74ed14
Jonathan M. Waldrop <relik107@gmail.com>
2022-07-25 15:31:56 -0500

    runtime to runtimeview

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,10 +1,10 @@
 #define CATCH_CONFIG_RUNNER
 #define CATCH_CONFIG_ENABLE_BENCHMARKING
 #include <catch2/catch.hpp>
-#include <parallelzone/runtime.hpp>
+#include <parallelzone/runtime/runtime_view.hpp>
 
 int main(int argc, char* argv[]) {
-    auto rt = parallelzone::Runtime(argc, argv);
+    auto rt = parallelzone::runtime::RuntimeView(argc, argv);
 
     int res = Catch::Session().run(argc, argv);
 
