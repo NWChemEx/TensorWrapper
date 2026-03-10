1bec0455f01518a508c9037cb166d0baa9d3394a
ryanmrichard <ryanmrichard@users.noreply.github.com>
2026-03-10 17:27:49 +0000

    deploy: 39cbf600022d6b75c9566794a83f605c972120f6

--- tensorwrapper_cxx_api/search/classes_1.js
+++ tensorwrapper_cxx_api/search/classes_1.js
@@ -1,5 +1,9 @@
 var searchData=
 [
   ['binaryoperationvisitor_0',['BinaryOperationVisitor',['../classtensorwrapper_1_1buffer_1_1detail___1_1BinaryOperationVisitor.html',1,'tensorwrapper::buffer::detail_']]],
-  ['bufferbase_1',['BufferBase',['../classtensorwrapper_1_1buffer_1_1BufferBase.html',1,'tensorwrapper::buffer']]]
+  ['bufferbase_1',['BufferBase',['../classtensorwrapper_1_1buffer_1_1BufferBase.html',1,'tensorwrapper::buffer']]],
+  ['bufferbasecommon_2',['BufferBaseCommon',['../classtensorwrapper_1_1buffer_1_1BufferBaseCommon.html',1,'tensorwrapper::buffer']]],
+  ['bufferbasecommon_3c_20bufferbase_20_3e_3',['BufferBaseCommon&lt; BufferBase &gt;',['../classtensorwrapper_1_1buffer_1_1BufferBaseCommon.html',1,'tensorwrapper::buffer']]],
+  ['bufferbasecommon_3c_20bufferviewbase_3c_20bufferbasetype_20_3e_20_3e_4',['BufferBaseCommon&lt; BufferViewBase&lt; BufferBaseType &gt; &gt;',['../classtensorwrapper_1_1buffer_1_1BufferBaseCommon.html',1,'tensorwrapper::buffer']]],
+  ['bufferviewbase_5',['BufferViewBase',['../classtensorwrapper_1_1buffer_1_1BufferViewBase.html',1,'tensorwrapper::buffer']]]
 ];
--- tensorwrapper_cxx_api/search/functions_7.js
+++ tensorwrapper_cxx_api/search/functions_7.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['has_5flayout_0',['has_layout',['../classtensorwrapper_1_1buffer_1_1BufferBase.html#a6623e82c5f3bef4b2fce767a55f55d40',1,'tensorwrapper::buffer::BufferBase']]],
+  ['has_5flayout_0',['has_layout',['../classtensorwrapper_1_1buffer_1_1BufferBaseCommon.html#aeb3c90e895104cfa13cc2c8be4087a6d',1,'tensorwrapper::buffer::BufferBaseCommon']]],
   ['has_5fobject_1',['has_object',['../classtensorwrapper_1_1dsl_1_1Labeled.html#a26ad5b68f450e36de28adb8836e329a3',1,'tensorwrapper::dsl::Labeled']]],
   ['has_5frepeated_5findices_2',['has_repeated_indices',['../classtensorwrapper_1_1dsl_1_1DummyIndices.html#a1967c3a0f0ecb5a34fe3ea913d40d09a',1,'tensorwrapper::dsl::DummyIndices']]],
   ['has_5fshape_3',['has_shape',['../structtensorwrapper_1_1detail___1_1TensorInput.html#acb1423af1f7c39ce8f21305a60a271eb',1,'tensorwrapper::detail_::TensorInput']]],
